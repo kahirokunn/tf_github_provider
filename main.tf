0be0fae44e6071024a625017049bf097ae372996
CRA Kahiro Okina <kahiro.okina@craftsman-software.com>
2022-08-29 11:43:03 +0900

    test

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "github" {}
+
+resource "github_repository" "foo" {
+  name      = "tf-acc-test-%s"
+  auto_init = true
+}
+
+resource "github_repository_file" "foo" {
+  repository          = github_repository.foo.name
+  branch              = "main"
+  file                = ".gitignore"
+  content             = "**/*.tfstate"
+  commit_message      = "Managed by Terraform"
+  commit_author       = "Terraform User"
+  commit_email        = "terraform@example.com"
+  overwrite_on_create = true
+}
